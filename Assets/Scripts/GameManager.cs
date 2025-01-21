fba83174f9c9d7e8330295666aa027f1a31eeb80
YJ402 <uky0916@gmail.com>
2025-01-21 16:02:31 +0900

    stageNum, toScene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
 using Unity.VisualScripting;
-using UnityEditor.Build.Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     private static GameManager _instance;
 
+    public int stageNum = 0;
     public static GameManager Instance
     {
         get
@@ -37,35 +34,13 @@ public class GameManager : MonoBehaviour
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
+
     }
 
 
-    public int stage = 0; // 스테이지 선택 값을 저장. 에셋이미지 불러올때 활용
 
-    public float bestScore = 0;
-    public float currentScore = 0;
-    PlayerPrefs playerPrefs;
-
-    public float[] scores = new float[5];
-
-
-
-    public void LoadScene(string SceneName)
+    public void LoadScene(string name)
     {
-        SceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene(name);
     }
-
-    /*
-    public void Score(string restTime)
-    {
-        currentScore = float.Parse(restTime);
-        if(currentScore > score[4])
-        {
-            score[4] = currentScore;
-            scores = scores.OrderBy(x => )
-
-        }
-
-    }
-    */
 }
