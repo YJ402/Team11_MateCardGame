5048800ca96a961b70ed7b8fbecde2e6eb4d7873
Gaon99 <shk9680@icloud.com>
2025-01-23 14:00:01 +0900

    asdf
    Succeess Scene Sound +
    Succ,Fail Scene Button Image +
    stage 1-5 sound +

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class BGM : MonoBehaviour
 {
     public AudioClip Stage1;
+    public AudioClip Stage2;
     public AudioClip Stage3;
+    public AudioClip Stage4;
     public AudioClip Stage5;
     GameManager GM;
     private SoundManager sound;
@@ -19,9 +21,32 @@ public class BGM : MonoBehaviour
         {
             audiosource = gameObject.AddComponent<AudioSource>();
         }
-        stage_type();
+        //stage_type();
+        sound_play();
+
     }
  
+    private void sound_play()
+    {
+        AudioClip Clip = null;
+        switch (GameManager.Instance.stageNum)
+        {
+            case 1: Clip = Stage1; break;
+            case 2: Clip = Stage2; break;
+            case 3: Clip = Stage3; break;
+            case 4: Clip = Stage4; break;
+            case 5: Clip = Stage5; break;
+        }
+        if(Clip != null)
+                {
+            audiosource.clip = Clip;
+            audiosource.Play();
+
+                }
+
+    }
+
+    /*
        public void stage_type()
     {
         AudioClip selectedClip = null;
@@ -48,5 +73,6 @@ public class BGM : MonoBehaviour
             audiosource.clip = selectedClip;
             audiosource.Play();
         }
-    }
+    */
 }
+
